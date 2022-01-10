c531b8d07834772849d9e2da7b7d2aa5173ec4f3
Linal180 <amirnisar75@gmail.com>
2022-01-10 13:00:56 +0500

    Implementing create-facility

--- src/components/main/facilities/facilitiesListing/index.tsx
+++ src/components/main/facilities/facilitiesListing/index.tsx
@@ -1,6 +1,25 @@
+// packages block
+import { FC } from 'react';
 // components block
 import FacilityTable from "./FacilityTable";
+import PageHeader from "../../../common/PageHeader";
+// constants block
+import { ADD_FACILITY, FACILITIES_BREAD, FACILITIES_ROUTE, FACILITIES_TEXT } from "../../../../constants";
 
-const FacilityComponent = (): JSX.Element => <FacilityTable />
+const FacilityComponent: FC = (): JSX.Element => {
+  return (
+    <>
+      <PageHeader
+        title={FACILITIES_TEXT}
+        path={[FACILITIES_BREAD]}
+        hasComponent
+        buttonText={ADD_FACILITY}
+        linkToPage={`${FACILITIES_ROUTE}/new`}
+      />
+
+      <FacilityTable />
+    </>
+  )
+}
 
 export default FacilityComponent;
