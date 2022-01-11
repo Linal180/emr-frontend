0fcee98d36a1b87695d49746e13ebb79a72578f3
Linal180 <amirnisar75@gmail.com>
2022-01-11 14:36:43 +0500

    remove unused code

--- src/validationSchemas/index.tsx
+++ src/validationSchemas/index.tsx
@@ -2,8 +2,7 @@
 import * as yup from "yup";
 // utils and constants block
 import { RequiredMessage } from "../utils";
-import { INVALID_EMAIL, REQUIRED_MESSAGE, ALPHABETS_REGEX, MaxLength, MinLength, NUMBER_REGEX, ValidMessage, PASSWORD_REGEX, PASSWORD_VALIDATION_MESSAGE, CONFIRM_YOUR_PASSWORD, PASSWORDS_MUST_MATCH, FIRST_NAME, LAST_NAME, PHONE_NUMBER, ZIP_CODE, EMAIL, PASSWORD_LABEL, NAME, PASSWORD, MOBILE_NUMBER, USER_NAME, DOB, GENDER, ROLE, FACILITY } from "../constants";
-import { yupResolver } from "@hookform/resolvers/yup";
+import { INVALID_EMAIL, ALPHABETS_REGEX, MaxLength, MinLength, NUMBER_REGEX, ValidMessage, PASSWORD_REGEX, PASSWORD_VALIDATION_MESSAGE, CONFIRM_YOUR_PASSWORD, PASSWORDS_MUST_MATCH, FIRST_NAME, LAST_NAME, PHONE_NUMBER, EMAIL, PASSWORD_LABEL, PASSWORD, MOBILE_NUMBER, USER_NAME, DOB, GENDER, ROLE, FACILITY } from "../constants";
 
 const emailSchema = { email: yup.string().email(INVALID_EMAIL).required(RequiredMessage(EMAIL)) };
 const passwordSchema = { password: yup.string().required(RequiredMessage(PASSWORD_LABEL)) }
@@ -11,14 +10,6 @@ const passwordSchema = { password: yup.string().required(RequiredMessage(PASSWOR
 const passwordAndRepeatPasswordSchema = {
   password: yup.string().required(RequiredMessage(PASSWORD)).matches(PASSWORD_REGEX, PASSWORD_VALIDATION_MESSAGE),
   repeatPassword: yup.string().oneOf([yup.ref("password"), null], PASSWORDS_MUST_MATCH).required(CONFIRM_YOUR_PASSWORD),
-}
-
-const basicUserSchema = {
-  firstName: yup.string().matches(ALPHABETS_REGEX, ValidMessage(FIRST_NAME)).min(3, MinLength(FIRST_NAME, 3)).max(26, MaxLength(FIRST_NAME, 26)).required(RequiredMessage(FIRST_NAME)),
-  lastName: yup.string().matches(ALPHABETS_REGEX, ValidMessage(LAST_NAME)).min(3, MinLength(LAST_NAME, 3)).max(26, MaxLength(LAST_NAME, 26)).required(RequiredMessage(LAST_NAME)),
-  phone: yup.string().matches(NUMBER_REGEX, ValidMessage(PHONE_NUMBER)).min(8, MinLength(PHONE_NUMBER, 8)).max(15, MaxLength(PHONE_NUMBER, 15)).required(RequiredMessage(PHONE_NUMBER)),
-  mobile: yup.string().matches(NUMBER_REGEX, ValidMessage(MOBILE_NUMBER)).min(8, MinLength(MOBILE_NUMBER, 8)).max(15, MaxLength(MOBILE_NUMBER, 15)).required(RequiredMessage(MOBILE_NUMBER)),
-  zipCode: yup.string().matches(NUMBER_REGEX, ValidMessage(ZIP_CODE)).required(RequiredMessage(ZIP_CODE))
 }
 
 export const loginValidationSchema = yup.object({
