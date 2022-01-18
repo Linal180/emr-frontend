0268e28aaaff01a6e1f47daf4be7d261a8eadae4
Linal180 <amirnisar75@gmail.com>
2022-01-18 13:16:50 +0500

    updated datepicker as per requirement

--- src/components/common/DatePicker.tsx
+++ src/components/common/DatePicker.tsx
@@ -1,14 +1,15 @@
-import 'date-fns';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Controller, useFormContext } from "react-hook-form";
 import { FormControl, InputLabel } from '@material-ui/core';
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
+import 'date-fns';
 // interfaces block
 import { VALID_DATE_REQUIRED } from "../../constants";
 import { DatePickerProps } from "../../interfacesTypes";
 
 const DatePicker: FC<DatePickerProps> = ({ name, label, error }): JSX.Element => {
+  const [openPicker, setOpenPicker] = useState<boolean>(false)
   const { control } = useFormContext()
 
   return (
@@ -24,15 +25,19 @@ const DatePicker: FC<DatePickerProps> = ({ name, label, error }): JSX.Element =>
 
           <MuiPickersUtilsProvider utils={DateFnsUtils}>
             <KeyboardDatePicker
-              value={field.value}
-              onChange={field.onChange}
-              error={invalid}
-              helperText={error && VALID_DATE_REQUIRED}
               id={`${name}-dialog`}
               variant="inline"
               format="dd/MM/yyyy"
               inputVariant="outlined"
               KeyboardButtonProps={{ 'aria-label': 'change date', }}
+              open={openPicker}
+              value={field.value}
+              onClick={() => setOpenPicker(!openPicker)}
+              onClose={() => setOpenPicker(!openPicker)}
+              onChange={field.onChange}
+              error={invalid}
+              helperText={error && VALID_DATE_REQUIRED}
+              autoOk
             />
           </MuiPickersUtilsProvider>
         </FormControl>
