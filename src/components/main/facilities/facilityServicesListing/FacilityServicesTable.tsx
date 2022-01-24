3ee51ebeb5c632aff185c44b6eb1683781646b6b
Linal180 <amirnisar75@gmail.com>
2022-01-24 19:03:15 +0500

    updating structure

--- src/components/main/facilities/facilityServicesListing/FacilityServicesTable.tsx
+++ src/components/main/facilities/facilityServicesListing/FacilityServicesTable.tsx
@@ -13,51 +13,49 @@ const FacilityServicesTable: FC = (): JSX.Element => {
   const classes = useTableStyles()
   const [searchQuery, setSearchQuery] = useState<string>('');
 
-
   const handleSearch = () => { }
   return (
-    <>
-      <Box className={classes.mainTableContainer}>
-        <Box className={classes.searchContainer}>
-          <TextField
-            value={searchQuery}
-            className={classes.tablesSearchIcon}
-            onChange={({ target: { value } }) => setSearchQuery(value)}
-            onKeyPress={({ key }) => key === "Enter" && handleSearch()}
-            name="searchQuery"
-            variant="outlined"
-            placeholder="Search"
-            fullWidth
-            InputProps={{
-              startAdornment:
-                <IconButton color="default">
-                  <TablesSearchIcon />
-                </IconButton>
-            }}
-          />
-        </Box>
+    <Box className={classes.mainTableContainer}>
+      <Box className={classes.searchContainer}>
+        <TextField
+          value={searchQuery}
+          className={classes.tablesSearchIcon}
+          onChange={({ target: { value } }) => setSearchQuery(value)}
+          onKeyPress={({ key }) => key === "Enter" && handleSearch()}
+          name="searchQuery"
+          variant="outlined"
+          placeholder="Search"
+          fullWidth
+          InputProps={{
+            startAdornment:
+              <IconButton color="default">
+                <TablesSearchIcon />
+              </IconButton>
+          }}
+        />
+      </Box>
 
-        <Box className="table-overflow">
-          <Table aria-label="customized table">
-            <TableHead>
-              <TableRow>
-                {renderTh(NAME)}
-                {renderTh(DURATION)}
-                {renderTh(PRICE)}
-                {renderTh(STATUS)}
-                {renderTh(ACTION, "center")}
-              </TableRow>
-            </TableHead>
+      <Box className="table-overflow">
+        <Table aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              {renderTh(NAME)}
+              {renderTh(DURATION)}
+              {renderTh(PRICE)}
+              {renderTh(STATUS)}
+              {renderTh(ACTION, "center")}
+            </TableRow>
+          </TableHead>
 
-            <TableBody>
-            </TableBody>
-          </Table>
-          <Box display="flex" justifyContent="center" pb={12} pt={5}>
-            <NoDataFoundComponent />
-          </Box>
+          <TableBody>
+          </TableBody>
+        </Table>
+
+        <Box display="flex" justifyContent="center" pb={12} pt={5}>
+          <NoDataFoundComponent />
         </Box>
       </Box>
-    </>
+    </Box>
   );
 };
 
