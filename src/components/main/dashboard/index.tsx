a333532b8dfb61176ab87e52067ff8624363d73c
Linal180 <amirnisar75@gmail.com>
2022-01-27 14:39:56 +0500

    updating dashboard as per figma

--- src/components/main/dashboard/index.tsx
+++ src/components/main/dashboard/index.tsx
@@ -1,12 +1,30 @@
-// components block
-import PageHeader from "../../common/PageHeader";
-// constants block
-import { DASHBOARD_TEXT } from "../../../constants";
+// packages block
+import { Box, Button, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+// svgs block
+import { EMRIcon } from "../../../assets/svgs";
+import DASHBOARD_IMAGE from "../../../assets/images/dashboard-image.svg"
 
 const DashboardComponent = (): JSX.Element => (
-  <PageHeader
-    title={DASHBOARD_TEXT}
-  />
+  <Box pt={5}>
+    <Box maxWidth={113} pb={2}>
+      <EMRIcon />
+    </Box>
+
+    <Box pb={1}>
+      <Typography component="h3" variant="h3">Welcome to EMR</Typography>
+    </Box>
+
+    <Box className="subHeading">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dolor interdum et risus. Accumsan.</Box>
+
+    <Link to="/">
+      <Button color="primary" variant="contained" className="blue-button">Start a Project</Button>
+    </Link>
+
+    <Box position="absolute" right={150} bottom={40}>
+      <img src={DASHBOARD_IMAGE} alt="" />
+    </Box>
+  </Box>
 );
 
 export default DashboardComponent;
