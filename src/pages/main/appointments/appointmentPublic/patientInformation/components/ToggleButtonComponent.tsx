6aa138ab4172a4c2696e6f5f00510a4abb4c1a40
Linal180 <amirnisar75@gmail.com>
2022-01-28 13:03:37 +0500

    minor fixes

--- src/pages/main/appointments/appointmentPublic/patientInformation/components/ToggleButtonComponent.tsx
+++ src/pages/main/appointments/appointmentPublic/patientInformation/components/ToggleButtonComponent.tsx
@@ -1,61 +1,14 @@
+// Packages block
 import { ChangeEvent, useState } from "react";
-import { Box, createStyles, FormControl, InputLabel, Switch, Theme, withStyles } from "@material-ui/core";
+import { Box, FormControl, InputLabel } from "@material-ui/core";
+// theme, interface/type and theme block
 import { RenderInputFieldProps } from "../../../../../../interfacesTypes";
-import { toggleButtonComponent } from "../../../../../../styles/publicAppointment/patientInformation";
-import { BLUE_SIX, GRAY_TWO, WHITE, WHITE_FIVE } from "../../../../../../theme";
+import { AntSwitch, toggleButtonComponent } from "../../../../../../styles/publicAppointment/patientInformation";
+import { GRAY_TWO, WHITE } from "../../../../../../theme";
 
 const ToggleButtonComponent = ({ name, label }: RenderInputFieldProps) => {
   const classes = toggleButtonComponent()
   const [isChecked, setIsChecked] = useState(false);
-
-  const AntSwitch = withStyles((theme: Theme) =>
-    createStyles({
-      root: {
-        width: '100%',
-        height: "100%",
-        padding: 5,
-        display: 'flex',
-        position: 'absolute',
-        left: 0,
-        top: 0,
-      },
-
-
-      thumb: {
-        width: 70,
-        height: 34,
-        opacity: 0.8,
-        borderRadius: 6,
-        backgroundColor: WHITE_FIVE,
-        boxShadow: 'none',
-        transform: 'translateX(93%)',
-      },
-
-      switchBase: {
-        padding: 4,
-
-        '&$checked': {
-          '& + $track': {
-            opacity: 0.8,
-            backgroundColor: 'transparent',
-            borderColor: 'none',
-          },
-
-          "& .MuiSwitch-thumb": {
-            backgroundColor: BLUE_SIX,
-            transform: 'translateX(-27%)',
-          }
-        },
-      },
-
-      track: {
-        border: `none`,
-        backgroundColor: WHITE,
-      },
-
-      checked: {},
-    }),
-  )(Switch);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { target: { checked } } = event
