6b6a10fab12254cd0bbcdfa849ed993dc6c3dcc4
Linal180 <amirnisar75@gmail.com>
2022-04-08 16:23:33 +0500

    temporary changes

--- src/components/main/patients/patientDetail/Insurance.tsx
+++ src/components/main/patients/patientDetail/Insurance.tsx
@@ -1,34 +1,44 @@
 // packages block
 import { useState } from 'react';
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
-import { Box, Button, Typography, Collapse, colors, Card, } from "@material-ui/core";
+import { ExpandMore } from '@material-ui/icons';
+import { Box, Typography, Collapse, colors, Card, Grid, Accordion, AccordionSummary, AccordionDetails, } from "@material-ui/core";
 // components block
 import Search from "../../../common/Search";
-import InputController from '../../../../controller';
+import Selector from '../../../common/Selector';
 // constant, utils and styles block
 import { BLUE_EIGHT, GRAY_TEN } from "../../../../theme";
 import { AddInsuranceIcon } from "../../../../assets/svgs";
 import { useTableStyles } from "../../../../styles/tableStyles";
 import {
-  ADD_POLICY, ADD_PRIMARY_INSURANCE, INSURANCE_POLICY_DETAILS, INSURANCE_SEARCH_DESCRIPTION, MEMBER_ID, 
-  MOST_USED_STANDARD_POLICES, PRIMARY_INSURANCE_DESCRIPTION, SEARCH
+  ADDITIONAL_DETAILS,
+  ADD_INSURANCE, ADD_INSURANCE_INFORMATION, COPAY_AMOUNTS, EMPTY_OPTION, EXPIRATION_DATE, INSURANCE_AND_POLICIES, INSURANCE_PAYER_NAME, INSURANCE_POLICY_DETAILS,
+  ISSUE_DATE,
+  MEMBER_ID_CERTIFICATE_NUMBER,
+  ORDER_OF_BENEFIT, PATIENT_RELATIONSHIP_TO_POLICY_HOLDER, POLICY_GROUP_NUMBER, POLICY_NAME,
 } from "../../../../constants";
+import InputController from '../../../../controller';
 
 const InsuranceComponent = (): JSX.Element => {
   const [open, setOpen] = useState<boolean>(false)
+  const [expanded, setExpanded] = useState<string | false>(false);
   const classes = useTableStyles();
   const methods = useForm<any>({
     mode: "all",
   });
   const { handleSubmit } = methods;
   const onSubmit: SubmitHandler<any> = () => { }
+
+  const handleChange = (panel: string) => (event: React.ChangeEvent<{}>, isExpanded: boolean) => {
+    setExpanded(isExpanded ? panel : false);
+  };
   return (
     <>
       <Card>
         <Box p={3}>
           <Collapse in={!open} mountOnEnter unmountOnExit>
             <Box onClick={() => setOpen(!open)}>
-              <Box py={2} borderBottom={`1px solid ${colors.grey[300]}`}>
+              <Box pb={2} mb={5} borderBottom={`1px solid ${colors.grey[300]}`}>
                 <Typography variant='h4'>{INSURANCE_POLICY_DETAILS}</Typography>
               </Box>
 
@@ -39,44 +49,129 @@ const InsuranceComponent = (): JSX.Element => {
                 <AddInsuranceIcon />
 
                 <Box pl={2}>
-                  <Typography component="h4" variant="h5">{ADD_PRIMARY_INSURANCE}</Typography>
-                  <Typography component="h5" variant="body1">{PRIMARY_INSURANCE_DESCRIPTION}</Typography>
+                  <Typography component="h4" variant="h5">{ADD_INSURANCE}</Typography>
+                  <Typography component="h5" variant="body2">{ADD_INSURANCE_INFORMATION}</Typography>
                 </Box>
               </Box>
             </Box>
           </Collapse>
 
           <Collapse in={open} mountOnEnter unmountOnExit>
-            <Box py={2} borderBottom={`1px solid ${colors.grey[300]}`}>
-              <Typography variant='h4'>{ADD_POLICY}</Typography>
+            <Box pb={2} borderBottom={`1px solid ${colors.grey[300]}`}>
+              <Typography variant='h4'>{INSURANCE_AND_POLICIES}</Typography>
             </Box>
 
             <Box pt={3} pb={5}>
               <FormProvider {...methods}>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                  <Box mb={3} display="flex" justifyContent="space-between" alignItems="flex-start" flexWrap="wrap">
-                    <Box className={classes.searchOuterContainer}>
-                      <Search search={Search} />
-                      <Box pl={3.5}>
-                        <Typography variant='body2'>{INSURANCE_SEARCH_DESCRIPTION}</Typography>
+                  <Grid container spacing={3}>
+                    <Grid item md={2} sm={12} xs={12}>
+                      <Box className={classes.searchOuterContainer} pb={0}>
+                        <Box pl={3.8}>
+                          <Typography variant='body1'>{INSURANCE_PAYER_NAME}</Typography>
+                        </Box>
+                        <Search search={Search} />
                       </Box>
-                    </Box>
+                    </Grid>
 
-                    <Box display="flex" alignItems="center">
-                      <InputController
-                        fieldType="text"
-                        controllerName="memberId"
-                        controllerLabel={MEMBER_ID}
+                    <Grid item md={2} sm={12} xs={12}>
+                      <Box className={classes.searchOuterContainer}>
+                        <Box pl={3.8}>
+                          <Typography variant='body1'>{POLICY_NAME}</Typography>
+                        </Box>
+                        <Search search={Search} />
+                      </Box>
+                    </Grid>
+
+                    <Grid item md={2} sm={12} xs={12}>
+                      <Typography variant='body1'>{ORDER_OF_BENEFIT}</Typography>
+                      <Selector
+                        name="orderBenefit"
+                        label={''}
+                        value={EMPTY_OPTION}
+                        options={[]}
                       />
+                    </Grid>
+                  </Grid>
 
-                      <Box ml={2} mt={2.5} alignSelf="flex-start">
-                        <Button variant='contained' color='primary' size='small'>{SEARCH}</Button>
-                      </Box>
-                    </Box>
-                  </Box>
 
-                  <Box mt={4} py={2} borderBottom={`1px solid ${colors.grey[300]}`}>
-                    <Typography variant='h4'>{MOST_USED_STANDARD_POLICES}</Typography>
+                  <Box mt={4}>
+
+                    <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')} className='accordionCustomize'>
+                      <AccordionSummary
+                        expandIcon={<ExpandMore />}
+                        aria-controls="panel1bh-content"
+                        id="panel1bh-header"
+                      >
+                        <Box pb={2} minWidth="100%" margin="auto" borderBottom={`1px solid ${colors.grey[300]}`}>
+                          <Typography variant='h4'>{ADDITIONAL_DETAILS}</Typography>
+                        </Box>
+                      </AccordionSummary>
+
+                      <AccordionDetails>
+                        <Box minWidth="100%" pt={3}>
+                          <Grid container spacing={3}>
+                            <Grid item md={4} sm={12} xs={12}>
+                              <Selector
+                                name="patientRelationship"
+                                label={PATIENT_RELATIONSHIP_TO_POLICY_HOLDER}
+                                value={EMPTY_OPTION}
+                                options={[]}
+                              />
+                            </Grid>
+
+                            <Grid item md={4} sm={12} xs={12}>
+                              <InputController
+                                fieldType="text"
+                                controllerName="memberId"
+                                controllerLabel={MEMBER_ID_CERTIFICATE_NUMBER}
+                              />
+                            </Grid>
+
+                            <Grid item md={4} sm={12} xs={12}>
+                              <InputController
+                                fieldType="text"
+                                controllerName="policyNumber"
+                                controllerLabel={POLICY_GROUP_NUMBER}
+                              />
+                            </Grid>
+
+                            <Grid item md={2} sm={12} xs={12}>
+                              <InputController
+                                fieldType="text"
+                                controllerName="issueDate"
+                                controllerLabel={ISSUE_DATE}
+                              />
+                            </Grid>
+
+                            <Grid item md={2} sm={12} xs={12}>
+                              <InputController
+                                fieldType="text"
+                                controllerName="expirationDate"
+                                controllerLabel={EXPIRATION_DATE}
+                              />
+                            </Grid>
+
+                            <Grid item md={4} sm={12} xs={12}>
+                              <Selector
+                                name="copayAmounts"
+                                label={COPAY_AMOUNTS}
+                                value={EMPTY_OPTION}
+                                options={[]}
+                              />
+                            </Grid>
+
+                            <Grid item md={2} sm={12} xs={12}>
+                              <InputController
+                                fieldType="text"
+                                controllerName="amount"
+                                controllerLabel={''}
+                              />
+                            </Grid>
+                          </Grid>
+                        </Box>
+                      </AccordionDetails>
+                    </Accordion>
                   </Box>
                 </form>
               </FormProvider>
