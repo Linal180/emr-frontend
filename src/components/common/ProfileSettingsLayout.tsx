3af2b0f310334e7b20666d2d6c78aced579d5e47
Linal180 <amirnisar75@gmail.com>
2022-04-12 10:05:07 +0500

    temporary commit

---
+++ src/components/common/ProfileSettingsLayout.tsx
@@ -0,0 +1,13 @@
+// packages block
+import { FC } from "react";
+import { Box, Typography } from "@material-ui/core";
+
+const ProfileSettingsLayout: FC = (): JSX.Element => {
+  return (
+    <>
+    hello
+    </>
+  );
+};
+
+export default ProfileSettingsLayout;
