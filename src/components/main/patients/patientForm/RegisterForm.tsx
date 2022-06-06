1f95a2a48a2b7a989931201000008c6c2582dce1
Linal180 <amirnisar75@gmail.com>
2022-06-06 15:24:49 +0500

    add-patient and copay modal modify

--- src/components/main/patients/patientForm/RegisterForm.tsx
+++ src/components/main/patients/patientForm/RegisterForm.tsx
@@ -1,21 +1,26 @@
 // package block
+import React, { FC, useState } from 'react';
 import { Box, Grid, List, ListItem, Typography } from '@material-ui/core';
 import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator } from '@material-ui/lab';
-import React, { FC, useState } from 'react';
-import { CONTACT_INFORMATION, CONTACT_INFORMATION_ROUTE, DEMOGRAPHICS, DEMOGRAPHICS_ROUTE, EMERGENCY_CONTACT, EMERGENCY_CONTACT_ROUTE, EMPLOYMENT, EMPLOYMENT_ROUTE, GUARANTOR, GUARANTOR_ROUTE, IDENTIFICATION, IDENTIFICATION_ROUTE, PRIVACY, PRIVACY__ROUTE, PROVIDER_REGISTRATION_DATES, PROVIDER_REGISTRATION__ROUTE, RegisterPatientMenuNav } from '../../../../constants';
-import { PatientCardsProps } from '../../../../interfacesTypes';
-import { useFacilityStyles } from '../../../../styles/facilityStyles';
-import ContactInfoCard from './ContactInfoCard';
-import PatientDemographicsCard from "./DemographicsCard";
-import EmergencyContactCard from './EmergencyContactCard';
-import EmploymentCard from './EmploymentCard';
 // component block
 import GuarantorCard from './GuarantorCard';
+import EmploymentCard from './EmploymentCard';
+import ContactInfoCard from './ContactInfoCard';
 import IdentificationCard from './IdentificationCard';
-import PatientGuardianCard from './PatientGuardianCard';
 import PatientNextKinCard from './PatientNextKinCard';
 import PatientPrivacyCard from './PatientPrivacyCard';
+import PatientDemographicsCard from "./DemographicsCard";
+import PatientGuardianCard from './PatientGuardianCard';
+import EmergencyContactCard from './EmergencyContactCard';
 import RegistrationDatesCard from './RegistrationDatesCard';
+// utils. interfaces, constants
+import { PatientCardsProps } from '../../../../interfacesTypes';
+import { useFacilityStyles } from '../../../../styles/facilityStyles';
+import { 
+  CONTACT_INFORMATION, CONTACT_INFORMATION_ROUTE, DEMOGRAPHICS, DEMOGRAPHICS_ROUTE, EMERGENCY_CONTACT, EMERGENCY_CONTACT_ROUTE, 
+  EMPLOYMENT, GUARANTOR, IDENTIFICATION, IDENTIFICATION_ROUTE, PRIVACY, PRIVACY__ROUTE, PROVIDER_REGISTRATION_DATES, 
+  PROVIDER_REGISTRATION__ROUTE, RegisterPatientMenuNav 
+} from '../../../../constants';
 
 const RegisterFormComponent: FC<PatientCardsProps> = ({ getPatientLoading, dispatch, isEdit, state, shouldDisableEdit }) => {
   const classes = useFacilityStyles()
@@ -92,25 +97,32 @@ const RegisterFormComponent: FC<PatientCardsProps> = ({ getPatientLoading, dispa
               <IdentificationCard getPatientLoading={getPatientLoading} shouldDisableEdit={shouldDisableEdit} />
             </Grid>
 
+            <Box pb={3} />
+
+            <Grid md={12} id={DEMOGRAPHICS_ROUTE} ref={demographicsRef}>
+              <PatientDemographicsCard getPatientLoading={getPatientLoading} state={state} dispatch={dispatch} shouldDisableEdit={shouldDisableEdit} />
+            </Grid>
 
             <Box pb={3} />
-            <Grid md={12} id={CONTACT_INFORMATION_ROUTE} ref={contactRef}>
+
+            <Grid md={12} id={CONTACT_INFORMATION_ROUTE} ref={contactRef} onTouchMove={()=>setActiveBlock(CONTACT_INFORMATION) }>
               <ContactInfoCard getPatientLoading={getPatientLoading} state={state} dispatch={dispatch} shouldDisableEdit={shouldDisableEdit} />
             </Grid>
 
-
             <Box pb={3} />
+
             <Grid md={12} id={PROVIDER_REGISTRATION__ROUTE} ref={providerRegisterationRef}>
               <RegistrationDatesCard getPatientLoading={getPatientLoading} isEdit={isEdit} state={state} shouldDisableEdit={shouldDisableEdit} />
             </Grid>
 
-
             <Box pb={3} />
+
             <Grid md={12} id={PRIVACY__ROUTE} ref={privacyRef}>
               <PatientPrivacyCard getPatientLoading={getPatientLoading} state={state} dispatch={dispatch} shouldDisableEdit={shouldDisableEdit} />
             </Grid>
 
             <Box pb={3} />
+
             <Grid md={12} id={EMERGENCY_CONTACT_ROUTE} ref={emergencyContactRef}>
               <EmergencyContactCard getPatientLoading={getPatientLoading} shouldDisableEdit={shouldDisableEdit} />
 
@@ -121,24 +133,15 @@ const RegisterFormComponent: FC<PatientCardsProps> = ({ getPatientLoading, dispa
               <Box pb={3} />
 
               <PatientGuardianCard getPatientLoading={getPatientLoading} shouldDisableEdit={shouldDisableEdit} />
-            </Grid>
 
-            <Box pb={3} />
-            <Grid md={12} id={GUARANTOR_ROUTE} ref={guarrenterRef}>
+              <Box pb={3} />
+
               <GuarantorCard getPatientLoading={getPatientLoading} state={state} dispatch={dispatch} shouldDisableEdit={shouldDisableEdit} />
-            </Grid>
 
-            <Box pb={3} />
-            <Grid md={12} id={EMPLOYMENT_ROUTE} ref={employmentRef}>
+              <Box pb={3} />
+
               <EmploymentCard getPatientLoading={getPatientLoading} shouldDisableEdit={shouldDisableEdit} />
-            </Grid>
-
-            <Box pb={3} />
-            <Grid md={12} id={DEMOGRAPHICS_ROUTE} ref={demographicsRef}>
-              <PatientDemographicsCard getPatientLoading={getPatientLoading} state={state} dispatch={dispatch} shouldDisableEdit={shouldDisableEdit} />
-            </Grid>
-
-            <Box pb={25} />
+            </Grid>s
           </Grid>
         </Box>
       </Box>
