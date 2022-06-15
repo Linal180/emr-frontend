792c8a58725dc0df88cf6777696766aaf2f58fde
Linal180 <amirnisar75@gmail.com>
2022-06-15 20:28:25 +0500

    charting feedback

--- src/components/main/patientChart/problems/modals/AddProblem.tsx
+++ src/components/main/patientChart/problems/modals/AddProblem.tsx
@@ -111,7 +111,7 @@ const AddAllergy: FC<AddAllergyModalProps> = ({ isOpen = false, handleModalClose
 
   const renderSearchData = useCallback(() => {
     return (
-      <Box maxHeight={200} className="overflowY-auto" display="flex"
+      <Box maxHeight={280} minHeight={260} className="overflowY-auto" display="flex"
         flexDirection="column" alignItems="flex-start"
       >
         {!!searchIcdCodesLoading ?
