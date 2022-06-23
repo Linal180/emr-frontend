6ffd0914f300d1b730cbd2927100106504bb1cb4
Linal180 <amirnisar75@gmail.com>
2022-06-23 13:24:03 +0500

    Updated form loading implemented for staff

--- src/components/common/PhoneInput.tsx
+++ src/components/common/PhoneInput.tsx
@@ -5,41 +5,44 @@ import { Controller, useFormContext } from 'react-hook-form';
 import { FormControl, InputLabel, FormHelperText } from "@material-ui/core";
 // styles utils and interface block
 import 'react-phone-input-2/lib/style.css'
-import { requiredLabel } from '../../utils';
 import { PhoneInputProps } from '../../interfacesTypes';
+import { renderLoading, requiredLabel } from '../../utils';
 
-const PhoneField: FC<PhoneInputProps> = ({ name, label, isRequired, disabled }) => {
+const PhoneField: FC<PhoneInputProps> = ({ name, label, isRequired, disabled, loading }) => {
   const { control } = useFormContext()
+  const inputLabel = isRequired ? requiredLabel(label) : label;
 
   return (
-    <Controller
-      rules={{ required: true }}
-      name={name}
-      control={control}
-      render={({ field, fieldState: { invalid, error: { message } = {} } }) => {
-        return <FormControl fullWidth margin='normal' error={invalid}>
-          <InputLabel id={`${name}-autocomplete`} shrink>
-            {isRequired ? requiredLabel(label) : label}
-          </InputLabel>
+    <>
+      {loading ? renderLoading(inputLabel || '') :
+        <Controller
+          rules={{ required: true }}
+          name={name}
+          control={control}
+          render={({ field, fieldState: { invalid, error: { message } = {} } }) => {
+            return <FormControl fullWidth margin='normal' error={invalid}>
+              <InputLabel id={`${name}-autocomplete`} shrink>
+                {inputLabel}
+              </InputLabel>
 
-          <PhoneInput
-            {...field}
-            country='us'
-            disableDropdown
-            disabled={disabled && true}
-            disableCountryCode
-            value={field.value}
-            onlyCountries={['us']}
-            placeholder='(111) 111-1111'
-            onChange={(phone: any) => field.onChange(phone)}
-            containerClass={invalid ? 'phoneInputError' : ''}
-            
-          />
+              <PhoneInput
+                {...field}
+                country='us'
+                disableDropdown
+                disabled={disabled && true}
+                disableCountryCode
+                value={field.value}
+                onlyCountries={['us']}
+                placeholder='(111) 111-1111'
+                onChange={(phone: any) => field.onChange(phone)}
+                containerClass={invalid ? 'phoneInputError' : ''}
+              />
 
-          <FormHelperText>{invalid && message}</FormHelperText>
-        </FormControl>;
-      }}
-    />
+              <FormHelperText>{invalid && message}</FormHelperText>
+            </FormControl>;
+          }}
+        />}
+    </>
   )
 }
 
