094b239899c4f07bd86ef3762d51291222a88849
Linal180 <amirnisar75@gmail.com>
2022-06-23 16:28:29 +0500

    multiline iframe

--- src/components/main/agreements/DocViewer.tsx
+++ src/components/main/agreements/DocViewer.tsx
@@ -6,22 +6,27 @@ import { DocViewerProps } from "../../../interfacesTypes";
 const DocViewer: FC<DocViewerProps> = ({ title, isOpen, handleClose, url }) => {
   var encodedUrl = encodeURIComponent(url || '')
   return (
-      <Dialog fullWidth maxWidth="sm" open={isOpen} onClose={handleClose}>
-        <DialogTitle>
-          <Typography variant="h4">{title}</Typography>
-        </DialogTitle>
+    <Dialog fullWidth maxWidth="sm" open={isOpen} onClose={handleClose}>
+      <DialogTitle>
+        <Typography variant="h4">{title}</Typography>
+      </DialogTitle>
 
-        <DialogContent>
-          <iframe title="Agreement" width="100%" height="600" frameBorder="0" src={`https://docs.google.com/viewer?url=${encodedUrl}&embedded=true`}></iframe>
+      <DialogContent>
+        <iframe
+          title="Agreement"
+          width="100%" height="600"
+          frameBorder="0"
+          src={`https://docs.google.com/viewer?url=${encodedUrl}&embedded=true`}
+        >
+        </iframe>
+      </DialogContent>
 
-        </DialogContent>
-
-        <DialogActions>
-          <Box display='flex' justifyContent='flex-end' alignItems='center'>
-            <Button variant='text' color='default' onClick={handleClose}>{CLOSE}</Button>
-          </Box>
-        </DialogActions>
-      </Dialog>
+      <DialogActions>
+        <Box display='flex' justifyContent='flex-end' alignItems='center'>
+          <Button variant='text' color='default' onClick={handleClose}>{CLOSE}</Button>
+        </Box>
+      </DialogActions>
+    </Dialog>
   )
 }
 
