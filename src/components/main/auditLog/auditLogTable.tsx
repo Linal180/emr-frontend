cb303872314517887ca4910134d96b5780ac83ad
Linal180 <amirnisar75@gmail.com>
2022-07-13 14:47:18 +0500

    remove day filters from audit listing

--- src/components/main/auditLog/auditLogTable.tsx
+++ src/components/main/auditLog/auditLogTable.tsx
@@ -1,4 +1,5 @@
 // packages
+import moment from "moment";
 import { CSVLink } from "react-csv";
 import { Link } from 'react-router-dom'
 import { Pagination } from "@material-ui/lab";
@@ -14,19 +15,16 @@ import UserSelector from "../../common/userLogs/UserSelector";
 import NoDataFoundComponent from "../../common/NoDataFoundComponent";
 import LogsPatientSelector from "../../common/userLogs/PatientSelector";
 //constants bock
-import { GRAY_SIX, } from "../../../theme";
 import { AuditLogsInputs } from "../../../interfacesTypes";
 import { useTableStyles } from "../../../styles/tableStyles";
-import { useChartingStyles } from '../../../styles/chartingStyles';
 import { useFindAllUserLogsLazyQuery, UserLogsPayload } from "../../../generated/graphql";
-import { formatModuleTypes, getFormatLogsDate, getFormatLogsTime, renderTh } from "../../../utils";
+import { formatModuleTypes, getFormatLogsDate, getFormatLogsTime, renderTh, setRecord } from "../../../utils";
 import { Action, State, initialState, userLogsReducer, ActionType } from '../../../reducers/userLogsReducer'
 import {
   ACTION, ALL_LOG_TYPES, DATE, DETAIL, FROM_DATE, IP_TEXT, AUDIT_LOG_REPORT, EXPORT_TO_FILE,
-  PATIENT, PATIENT_NAME, TIME, TO_DATE, TYPE, UPDATE_FILTER, USER_NAME, USER_TEXT,
-  AUDIT_TIME_ENUMS, USER_LOG_PAGE_LIMIT, MODULE_LOGS_TYPES, PAGE_LIMIT, PATIENTS_ROUTE
+  PATIENT, PATIENT_NAME, TIME, TO_DATE, TYPE, UPDATE_FILTER, USER_NAME, USER_TEXT, USER_LOG_PAGE_LIMIT,
+  MODULE_LOGS_TYPES, PAGE_LIMIT, PATIENTS_ROUTE, RESET
 } from "../../../constants";
-import moment from "moment";
 
 const headers = [
   { label: "Date", key: "date" },
@@ -46,24 +44,29 @@ const AuditLogTable = (): JSX.Element => {
       startDate: new Date().toString()
     }
   });
-  const chartingClasses = useChartingStyles();
-  const [state, dispatch] = useReducer<Reducer<State, Action>>(userLogsReducer, initialState)
 
-  const { timeDuration, page, totalPages, userLogs, csvData } = state
+  const [state, dispatch] = useReducer<Reducer<State, Action>>(userLogsReducer, initialState)
+  const { page, totalPages, userLogs, csvData } = state
   const { handleSubmit, getValues, setValue } = methods
   const { patient, module, user, startDate, endDate } = getValues()
 
   const [findAllUserLogs, { loading, error }] = useFindAllUserLogsLazyQuery({
+    fetchPolicy: "network-only",
+    nextFetchPolicy: 'no-cache',
+    notifyOnNetworkStatusChange: true,
+
     onCompleted: (data) => {
       const { findAllUserLogs } = data || {}
       const { response, pagination, userLogs } = findAllUserLogs || {}
       const { status } = response || {}
+
       if (status === 200) {
         const { totalPages } = pagination || {}
         dispatch({ type: ActionType.SET_TOTAL_PAGES, totalPages: totalPages ?? 0 })
         userLogs && dispatch({ type: ActionType.SET_USER_LOGS, userLogs: userLogs as UserLogsPayload['userLogs'] })
       }
     },
+
     onError: () => {
       dispatch({ type: ActionType.SET_PAGE, page: 1 })
       dispatch({ type: ActionType.SET_TOTAL_PAGES, totalPages: 0 })
@@ -110,43 +113,13 @@ const AuditLogTable = (): JSX.Element => {
     } catch (error) { }
   }
 
-  const dateHandler = (timeKey: string) => {
-    switch (timeKey) {
-      case 'Day':
-        setValue('endDate', new Date()?.toString())
-        setValue('startDate', new Date(new Date()?.setDate(new Date().getDate() - 1))?.toString())
-        break;
-
-      case 'Week':
-        setValue('endDate', new Date()?.toString())
-        setValue('startDate', new Date(new Date().setDate(new Date().getDate() - 7))?.toString())
-        break;
-
-      case 'Month':
-
-        setValue('endDate', new Date()?.toString())
-        setValue('startDate', new Date(new Date().setMonth(new Date().getMonth() - 1))?.toString())
-        break;
-
-      case 'Year':
-        setValue('endDate', new Date()?.toString())
-        setValue('startDate', new Date(new Date().setFullYear(new Date().getFullYear() - 1))?.toString())
-        break;
-
-
-      default:
-        setValue('endDate', new Date()?.toString())
-        setValue('startDate', new Date(new Date()?.setDate(new Date().getDate() - 1))?.toString())
-        break;
-    }
-  }
-
   useMemo(() => {
     if (!!userLogs?.length) {
       const arr = userLogs?.map((item) => {
         const { createdAt, moduleType, user, patient, operationType } = item || {}
         const { email } = user || {}
         const { firstName, lastName } = patient || {}
+
         return {
           user: email || '',
           moduleType: moduleType || "",
@@ -159,6 +132,15 @@ const AuditLogTable = (): JSX.Element => {
       arr?.length && dispatch({ type: ActionType.SET_CSV_DATA, csvData: arr })
     }
   }, [userLogs])
+
+  const resetHandler = async () => {
+    setValue('patient', setRecord('', '--'));
+    setValue('user', setRecord('', '--'));
+    setValue('module', setRecord('', '--'));
+    setValue('startDate', new Date().toString());
+    setValue('endDate', new Date().toString());
+    await fetchAllUserLogs()
+  }
 
   return (
     <Fragment>
@@ -176,7 +158,7 @@ const AuditLogTable = (): JSX.Element => {
       <Box mt={3} mb={1}>
         <FormProvider {...methods}>
           <Grid container spacing={3} direction='row'>
-            <Grid item md={10} sm={12} xs={12}>
+            <Grid item md={12} sm={12} xs={12}>
               <Grid container spacing={3} direction='row'>
                 <Grid item md={2} sm={12} xs={12}>
                   <LogsPatientSelector
@@ -212,31 +194,19 @@ const AuditLogTable = (): JSX.Element => {
                 </Grid>
 
                 <Grid item md={2} sm={12} xs={12}>
-                  <Box mt={2.5}>
-                    <Button variant="contained" color="secondary" onClick={handleSubmit(onSubmit)}>{UPDATE_FILTER}</Button>
+                  <Box display={'flex'} justifyContent={'space-between'}>
+                    <Box mt={2.5}>
+                      <Button variant="contained" color="secondary" onClick={handleSubmit(onSubmit)}>{UPDATE_FILTER}</Button>
+                    </Box>
+
+                    <Box mt={2.5}>
+                      <Button variant="outlined" color="default" onClick={resetHandler}>{RESET}</Button>
+                    </Box>
                   </Box>
                 </Grid>
               </Grid>
             </Grid>
 
-            <Grid item md={2} sm={12} xs={12}>
-              <Box className={`${chartingClasses.toggleProblem} ${chartingClasses.toggleBox}`}>
-                <Box display='flex' border={`1px solid ${GRAY_SIX}`} borderRadius={6} mt={2}>
-                  {AUDIT_TIME_ENUMS?.map((timeKey, index) => {
-                    return (<Box key={`${timeKey}-${index}`}
-                      className={timeKey === timeDuration ? 'selectedBox selectBox' : 'selectBox'}
-                      onClick={() => {
-                        dispatch({ type: ActionType.SET_TIME_DURATION, timeDuration: timeKey })
-                        dateHandler(timeKey)
-                      }}
-                    >
-                      <Typography variant='h6'>{timeKey}</Typography>
-                    </Box>
-                    )
-                  })}
-                </Box>
-              </Box>
-            </Grid>
           </Grid>
         </FormProvider>
       </Box>
