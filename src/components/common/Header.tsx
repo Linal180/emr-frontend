5d699f91940501cc4e2d03913d0ae1f399cab16c
Linal180 <amirnisar75@gmail.com>
2022-07-25 14:29:42 +0500

    design in progress

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,11 @@
 // packages block
 import { FC, useContext, useState, MouseEvent } from "react";
 import { Link } from "react-router-dom";
+import { BLACK } from "../../theme";
 import { Menu as MenuIcon } from "@material-ui/icons";
-import { AppBar, Typography, Box, Toolbar, IconButton, Menu, MenuItem, Fade, } from '@material-ui/core';
+import { 
+  AppBar, Typography, Box, Toolbar, IconButton, Menu, MenuItem, Fade, 
+} from '@material-ui/core';
 // Components block
 import DropdownMenu from "./DropdownMenu";
 import ProfileDropdownMenu from "./ProfileDropdownMenu";
@@ -14,10 +17,9 @@ import { useHeaderStyles } from "../../styles/headerStyles";
 import { activeClass, checkPermission, getHigherRole, isSuperAdmin, isUserAdmin } from "../../utils";
 import {
   APPOINTMENT_MENU_ITEMS, FACILITIES_TEXT, SUPER_ADMIN, USER_PERMISSIONS, FACILITIES_ROUTE, ROOT_ROUTE,
-  PRACTICE_MANAGEMENT_TEXT, PRACTICE_MANAGEMENT_ROUTE, SETTINGS_ROUTE, SCHEDULE_TEXT, HOME_TEXT, PATIENTS_ROUTE,
-  PATIENTS_TEXT,
+  PRACTICE_MANAGEMENT_TEXT, PRACTICE_MANAGEMENT_ROUTE, SETTINGS_ROUTE, SCHEDULE_TEXT, HOME_TEXT, 
+  PATIENTS_ROUTE, PATIENTS_TEXT, BILLING_TEXT, BILLING_MENU_ITEMS,
 } from "../../constants";
-import { BLACK } from "../../theme";
 
 const Header: FC = (): JSX.Element => {
   const classes = useHeaderStyles();
@@ -91,11 +93,11 @@ const Header: FC = (): JSX.Element => {
             </Typography>
           }
 
-          {/* <DropdownMenu
+          <DropdownMenu
             itemName={BILLING_TEXT}
             menuItem={BILLING_MENU_ITEMS}
             current={currentRoute === 'inBilling'}
-          /> */}
+          />
 
           {checkPermission(userPermissions, USER_PERMISSIONS.findAllFacility)
             && showFacility &&
@@ -176,11 +178,11 @@ const Header: FC = (): JSX.Element => {
                 </Typography>
               }
 
-              {/* <DropdownMenu
+              <DropdownMenu
                       itemName={BILLING_TEXT}
                       menuItem={BILLING_MENU_ITEMS}
                       current={currentRoute === 'inBilling'}
-                    /> */}
+                    />
 
               {checkPermission(userPermissions, USER_PERMISSIONS.findAllFacility)
                 && showFacility &&
--- src/components/main/billing/claimFeedListing/ClaimFeedTable.tsx
+++ src/components/main/billing/claimFeedListing/ClaimFeedTable.tsx
@@ -1,46 +1,84 @@
 // packages block
 import { FC, } from "react";
-import { Box, Table, TableBody, TableHead, TableRow } from "@material-ui/core";
+import { Box, Button, Table, TableBody, TableHead, TableRow, Typography } from "@material-ui/core";
 // components block
 import NoDataFoundComponent from "../../../common/NoDataFoundComponent";
 // graphql, constants, context, interfaces/types, reducer, svgs and utils block
 import { renderTh } from "../../../../utils";
-import { PATIENT, VISIT, PROVIDER, FACILITY, BILLED, CLAIMED, STATUS } from "../../../../constants";
+import { PATIENT, VISIT, PROVIDER, FACILITY, BILLED, CLAIMED, STATUS, CLAIM_FEED_TEXT, EXPORT_TO_FILE, SCHEDULE } from "../../../../constants";
 import { useTableStyles } from "../../../../styles/tableStyles";
-import Search from "../../../common/Search";
+import { AddWhiteIcon } from "../../../../assets/svgs";
+import InputController from "../../../../controller";
+import { FormProvider, useForm } from "react-hook-form";
 
 const ClaimFeedTable: FC = (): JSX.Element => {
   const classes = useTableStyles()
-  const search = (query: string) => { }
+
+  const methods = useForm({
+    mode: "all",
+  });
 
   return (
-    <Box className={classes.mainTableContainer}>
-      <Box mb={2} maxWidth={450}>
-        <Search search={search} />
-      </Box>
+    <>
+      <Box mb={2} display='flex' justifyContent='space-between' alignItems='center'>
+        <Typography variant="h4" color="textPrimary">{CLAIM_FEED_TEXT}</Typography>
 
-      <Box className="table-overflow">
-        <Table aria-label="customized table">
-          <TableHead>
-            <TableRow>
-              {renderTh(PATIENT)}
-              {renderTh(VISIT)}
-              {renderTh(PROVIDER)}
-              {renderTh(FACILITY)}
-              {renderTh(BILLED)}
-              {renderTh(CLAIMED)}
-              {renderTh(STATUS)}
-            </TableRow>
-          </TableHead>
-          <TableBody>
+        <Box display='flex'>
+          <Button
+            variant="contained"
+            color="secondary"
+          >
+            {EXPORT_TO_FILE}
+          </Button>
 
-          </TableBody>
-        </Table>
-        <Box display="flex" justifyContent="center" alignItems="center" pb={12} pt={5}>
-          <NoDataFoundComponent />
+          <Box p={1} />
+
+          <Button
+            variant="contained" color="primary"
+          >
+            <AddWhiteIcon />
+            <Box ml={1} />
+            {SCHEDULE}
+          </Button>
         </Box>
       </Box>
-    </Box>
+
+      <FormProvider {...methods}>
+        <form>
+          <Box display="flex" alignItems="center">
+            <InputController
+              fieldType="text"
+              controllerName="npi"
+              controllerLabel={'NPI'}
+            />
+          </Box>
+        </form>
+      </FormProvider>
+
+      <Box className={classes.mainTableContainer}>
+        <Box className="table-overflow">
+          <Table aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                {renderTh(PATIENT)}
+                {renderTh(VISIT)}
+                {renderTh(PROVIDER)}
+                {renderTh(FACILITY)}
+                {renderTh(BILLED)}
+                {renderTh(CLAIMED)}
+                {renderTh(STATUS)}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+
+            </TableBody>
+          </Table>
+          <Box display="flex" justifyContent="center" alignItems="center" pb={12} pt={5}>
+            <NoDataFoundComponent />
+          </Box>
+        </Box>
+      </Box>
+    </>
   );
 };
 
