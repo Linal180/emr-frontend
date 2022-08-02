dfcff6e63a1127cb833dae1b69b7a721d2c6235d
Linal180 <amirnisar75@gmail.com>
2022-08-02 11:30:13 +0500

    inprogress

--- src/components/main/facilities/services/serviceListing/index.tsx
+++ src/components/main/facilities/services/serviceListing/index.tsx
@@ -9,6 +9,10 @@ import { ParamsType } from '../../../../../interfacesTypes';
 import {
   ADD_SERVICE, FACILITIES_BREAD, FACILITIES_ROUTE, FACILITY_SERVICES_ROUTE, FACILITY_SERVICES_TEXT, SERVICES
 } from "../../../../../constants";
+import BackButton from '../../../../common/BackButton';
+import { Box, Button, } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { AddWhiteIcon } from '../../../../../assets/svgs';
 
 const FacilityServicesComponent: FC = (): JSX.Element => {
   const { id } = useParams<ParamsType>();
@@ -16,13 +20,26 @@ const FacilityServicesComponent: FC = (): JSX.Element => {
 
   return (
     <>
-      <PageHeader
-        hasComponent
-        buttonText={ADD_SERVICE}
-        title={FACILITY_SERVICES_TEXT}
-        path={[FACILITIES_BREAD, servicesBreadcrumb]}
-        linkToPage={`${FACILITIES_ROUTE}/${id}${FACILITY_SERVICES_ROUTE}/new`}
-      />
+      <Box display='flex' justifyContent='space-between' alignItems='center'>
+        <Box display='flex'>
+          <BackButton to={FACILITIES_ROUTE} />
+
+          <Box ml={2} />
+
+          <PageHeader
+            title={FACILITY_SERVICES_TEXT}
+            path={[FACILITIES_BREAD, servicesBreadcrumb]}
+          />
+        </Box>
+
+        <Link to={`${FACILITIES_ROUTE}/${id}${FACILITY_SERVICES_ROUTE}/new`}>
+          <Button type="submit" variant="contained" color="primary">
+            <AddWhiteIcon />
+            <Box p={0.5} />
+            {ADD_SERVICE}
+          </Button>
+        </Link>
+      </Box>
 
       <ServicesTable />
     </>
