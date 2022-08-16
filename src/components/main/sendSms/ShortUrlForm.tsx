a2671ac22ce52bca29f6c4d3bec9bfd738cafb4a
Linal180 <amirnisar75@gmail.com>
2022-08-16 12:49:07 +0500

    remove extra spacing

--- src/components/main/sendSms/ShortUrlForm.tsx
+++ src/components/main/sendSms/ShortUrlForm.tsx
@@ -11,71 +11,67 @@ import { shortUrlSchema } from "../../../validationSchemas";
 import { LONG_URL_TEXT, SHORT_URL_TEXT } from "../../../constants";
 import { useCreateShortUrlMutation } from "../../../generated/graphql";
 
-
 const ShortUrlForm: FC = (): JSX.Element => {
+  const [link, setLink] = useState<string>('');
+  const methods = useForm<ShortUrlFormType>({
+    resolver: yupResolver(shortUrlSchema),
+    defaultValues: { longUrl: "", }
+  });
 
-	const [link, setLink] = useState<string>('')
+  const { handleSubmit } = methods;
 
-	const methods = useForm<ShortUrlFormType>({
-		resolver: yupResolver(shortUrlSchema),
-		defaultValues: { longUrl: "", }
-	});
-	const { handleSubmit } = methods;
+  const [createShortUrl, { loading }] = useCreateShortUrlMutation({
+    onCompleted: (data) => {
+      const { createShortUrl } = data;
+      const { response, shortUrl } = createShortUrl || {}
+      const { message, status } = response || {}
+      if (status === 200) {
+        message && Alert.success(message)
+        const { shortLink } = shortUrl || {}
+        shortLink && setLink(shortLink)
+      }
+    },
+    onError: (error) => {
+      Alert.error(error?.message)
+    }
+  })
 
-	const [createShortUrl, { loading }] = useCreateShortUrlMutation({
-		onCompleted: (data) => {
-			const { createShortUrl } = data;
-			const { response, shortUrl } = createShortUrl || {}
-			const { message, status } = response || {}
-			if (status === 200) {
-				message && Alert.success(message)
-				const { shortLink } = shortUrl || {}
-				shortLink && setLink(shortLink)
-			}
-		},
-		onError: (error) => {
-			Alert.error(error?.message)
-		}
-	})
+  const onSubmit: SubmitHandler<ShortUrlFormType> = async (values) => {
+    try {
+      const { longUrl } = values;
+      await createShortUrl({ variables: { createShortUrlInput: { longLink: longUrl } } })
+    } catch (error) { }
+  }
 
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={3}>
 
-	const onSubmit: SubmitHandler<ShortUrlFormType> = async (values) => {
-		try {
-			const { longUrl } = values;
-			await createShortUrl({ variables: { createShortUrlInput: { longLink: longUrl } } })
-		} catch (error) { }
-	}
-
-	return (
-		<FormProvider {...methods}>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<Grid container spacing={3}>
-
-					<Grid item xs={12} sm={12} md={12}>
-						<InputController
-							isRequired
-							rows={4}
-							controllerLabel={LONG_URL_TEXT}
-							controllerName="longUrl"
-						/>
-					</Grid>
-					{link && <Grid item xs={12} sm={12} md={12}>
-						<Typography>
-							{link}
-						</Typography>
-					</Grid>}
-					<Grid item xs={12} sm={12} md={12}>
-						<Box display='flex' justifyContent='flex-start' alignItems="baseline">
-							<Button type='submit' variant='contained' color='primary' disabled={loading}>
-								{(loading) && <CircularProgress size={20} color="inherit" />} {SHORT_URL_TEXT}
-							</Button>
-						</Box>
-					</Grid>
-				</Grid>
-			</form>
-		</FormProvider>
-	)
-
+          <Grid item xs={12} sm={12} md={12}>
+            <InputController
+              isRequired
+              rows={4}
+              controllerLabel={LONG_URL_TEXT}
+              controllerName="longUrl"
+            />
+          </Grid>
+          {link && <Grid item xs={12} sm={12} md={12}>
+            <Typography>
+              {link}
+            </Typography>
+          </Grid>}
+          <Grid item xs={12} sm={12} md={12}>
+            <Box display='flex' justifyContent='flex-start' alignItems="baseline">
+              <Button type='submit' variant='contained' color='primary' disabled={loading}>
+                {(loading) && <CircularProgress size={20} color="inherit" />} {SHORT_URL_TEXT}
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </form>
+    </FormProvider>
+  )
 }
 
 export default ShortUrlForm
