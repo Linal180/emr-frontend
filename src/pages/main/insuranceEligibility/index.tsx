096f925d978a5d2fc4e8797f1da8415f0798fa6b
Linal180 <amirnisar75@gmail.com>
2022-09-05 18:57:40 +0500

    add insurance eligibility check

---
+++ src/pages/main/insuranceEligibility/index.tsx
@@ -0,0 +1,6 @@
+//packages block
+import { FC } from "react";
+// components block
+import InsuranceEligibilityComponent from "../../../components/main/insuranceEligibility";
+
+export const InsuranceEligibility: FC = (): JSX.Element => <InsuranceEligibilityComponent />;
