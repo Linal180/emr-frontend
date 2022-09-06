0325e2966581f4c89f68a9cb4d0f3a99ccc2f834
Linal180 <amirnisar75@gmail.com>
2022-09-06 18:36:02 +0500

    external appointment ui issues fix

--- src/components/common/formBuilder/ContractForm.tsx
+++ src/components/common/formBuilder/ContractForm.tsx
@@ -9,10 +9,10 @@ import { CONTRACT_NO, ORGANIZATION_NAME } from "../../../constants"
 const ContractForm: FC = (): JSX.Element => {
   return <Box my={3}>
     <Grid container spacing={3}>
-      <Grid item xs={3}>
+      <Grid item md={3} sm={6} xs={12}>
         <InputController controllerName="contractNumber" fieldType="text" controllerLabel={CONTRACT_NO} isRequired />
       </Grid>
-      <Grid item xs={3}>
+      <Grid item md={3} sm={6} xs={12}>
         <InputController controllerName="organizationName" fieldType="text" controllerLabel={ORGANIZATION_NAME} isRequired />
       </Grid>
     </Grid>
