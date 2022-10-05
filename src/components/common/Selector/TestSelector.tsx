76e7f7b9e6ca8a3a8c9bf628a268d4214f7a7b01
Linal180 <amirnisar75@gmail.com>
2022-10-05 15:58:38 +0500

    appointmentAndLaborderFix

--- src/components/common/Selector/TestSelector.tsx
+++ src/components/common/Selector/TestSelector.tsx
@@ -6,7 +6,7 @@ import { Controller, useFormContext } from "react-hook-form";
 // components block
 import AutocompleteTextField from "../AutocompleteTextField";
 // utils and interfaces/types block
-import { DROPDOWN_PAGE_LIMIT, EMPTY_OPTION_WITHOUT_DASHES } from "../../../constants";
+import { DROPDOWN_PAGE_LIMIT, EMPTY_OPTION } from "../../../constants";
 import { LoincCodesPayload, useFindAllLoincCodesLazyQuery } from "../../../generated/graphql";
 import { FacilitySelectorProps, SelectorOption } from "../../../interfacesTypes";
 import { renderTests, requiredLabel } from "../../../utils";
@@ -15,7 +15,7 @@ const TestsSelector: FC<FacilitySelectorProps> = ({ name, label, disabled, isReq
   const { control } = useFormContext()
   const [searchQuery, setSearchQuery] = useState<string>('')
   const [loincCodes, setLoincCodes] = useState<LoincCodesPayload['loincCodes']>([])
-  const updatedOptions = addEmpty ? [EMPTY_OPTION_WITHOUT_DASHES, ...renderTests(loincCodes ?? [])] : [...renderTests(loincCodes ?? [])]
+  const updatedOptions = addEmpty ? [EMPTY_OPTION, ...renderTests(loincCodes ?? [])] : [...renderTests(loincCodes ?? [])]
 
   const [findAllLoincCodes, { loading }] = useFindAllLoincCodesLazyQuery({
     notifyOnNetworkStatusChange: true,
