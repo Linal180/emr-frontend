92a2ab232b59af9a480e61d1947099873dd02151
Linal180 <amirnisar75@gmail.com>
2022-10-27 14:46:35 +0500

    add infinite scroll in family history

--- src/reducers/familyHistoryFormReducer.ts
+++ src/reducers/familyHistoryFormReducer.ts
@@ -2,26 +2,34 @@ import { IcdCodesPayload } from "../generated/graphql";
 
 
 export interface State {
+	page: number;
 	problem: string;
+	totalPages: number;
 	searchQuery: string;
 	searchedData: IcdCodesPayload['icdCodes']
 }
 
 
 export const initialState: State = {
+	page: 1,
 	problem: '',
+	totalPages: 0,
 	searchQuery: '',
 	searchedData: []
 }
 
 export enum ActionType {
+	SET_PAGE = 'setPage',
 	SET_PROBLEM = 'setProblem',
+	SET_TOTAL_PAGES = 'setTotalPages',
 	SET_SEARCH_QUERY = 'setSearchQuery',
-	SET_SEARCHED_DATA = 'setSearchData'
+	SET_SEARCHED_DATA = 'setSearchData',
 }
 
 export type Action =
+	| { type: ActionType.SET_PAGE; page: number }
 	| { type: ActionType.SET_PROBLEM; problem: string }
+	| { type: ActionType.SET_TOTAL_PAGES; totalPages: number }
 	| { type: ActionType.SET_SEARCH_QUERY; searchQuery: string }
 	| { type: ActionType.SET_SEARCHED_DATA, searchedData: IcdCodesPayload['icdCodes'] }
 
@@ -45,6 +53,18 @@ export const familyHistoryFormReducer = (state: State, action: Action): State =>
 			return {
 				...state,
 				searchedData: action.searchedData
+			}
+
+		case ActionType.SET_PAGE:
+			return {
+				...state,
+				page: action.page
+			}
+
+		case ActionType.SET_TOTAL_PAGES:
+			return {
+				...state,
+				totalPages: action.totalPages
 			}
 	}
 }
